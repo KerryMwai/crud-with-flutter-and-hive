a28995a71ef37a8f394c6919b092898180b29a06
KerryMwai <kerrywangamati@gmail.com>
2023-06-02 12:49:29 +0300

    Placed the form dialog into its own file from the main file

--- lib/core/widgets/form_dialog_widget.dart
+++ lib/core/widgets/form_dialog_widget.dart
@@ -1,93 +1,95 @@
-// import 'package:flutter/material.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
-// class ShoppingHiveForm{
-//   final TextEditingController nameController;
-//   final TextEditingController quantityController;
-//   final BuildContext context;
+import '../controller/shopping_box_controller.dart';
 
-//    ShoppingHiveForm({required this.nameController, required this.quantityController, required this.context});
+class ShoppingHiveFormDialog{
+  final TextEditingController nameController;
+  final TextEditingController quantityController;
+  final BuildContext context;
+
+   ShoppingHiveFormDialog({required this.nameController, required this.quantityController, required this.context});
 
 
-//     Future<void> _showForm(key) async {
-//     if (key != null) {
-//       final existingItem = _items.firstWhere((item) => item['key'] == key);
+     Future<void> showForm(key) async {
+    if (key != null) {
+      final existingItem =Get.find<ShoppingBoxController>().items.firstWhere((item) => item['key'] == key);
 
-//       nameController.text = existingItem['name'];
-//       quantityController.text = existingItem['quantity'];
-//     }
+      nameController.text = existingItem['name'];
+      quantityController.text = existingItem['quantity'];
+    }
 
-//     if (key == null) {
-//       nameController.clear();
-//       quantityController.clear();
-//     }
+    if (key == null) {
+      nameController.clear();
+      quantityController.clear();
+    }
 
-//     return showDialog<void>(
-//       context: context,
-//       barrierDismissible: false, // user must tap button!
-//       builder: (BuildContext context) {
-//         return AlertDialog(
-//           title: key == null
-//               ? const Text('Add shopping item')
-//               : const Text('Edit shopping item'),
-//           content: SingleChildScrollView(
-//             child: ListBody(
-//               children: <Widget>[
-//                 TextField(
-//                   controller: nameController,
-//                   decoration: const InputDecoration(hintText: "Name"),
-//                 ),
-//                 const SizedBox(
-//                   height: 10,
-//                 ),
-//                 TextField(
-//                   controller: quantityController,
-//                   keyboardType: TextInputType.number,
-//                   decoration: const InputDecoration(hintText: "Quatity"),
-//                 ),
-//                 const SizedBox(
-//                   height: 10,
-//                 ),
-//                 const SizedBox(
-//                   height: 15,
-//                 ),
-//               ],
-//             ),
-//           ),
-//           actions: <Widget>[
-//             TextButton(
-//               child: const Text('Cancel'),
-//               onPressed: () {
-//                 Navigator.of(context).pop();
-//               },
-//             ),
-//             TextButton(
-//                 child: key == null
-//                     ? const Text('Add Item')
-//                     : const Text('Update Item'),
-//                 onPressed: () {
-//                   if (key == null) {
-//                     _createItem({
-//                       "name": nameController.text,
-//                       "quantity": quantityController.text
-//                     });
+    return showDialog<void>(
+      context: context,
+      barrierDismissible: false, // user must tap button!
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: key == null
+              ? const Text('Add shopping item')
+              : const Text('Edit shopping item'),
+          content: SingleChildScrollView(
+            child: ListBody(
+              children: <Widget>[
+                TextField(
+                  controller: nameController,
+                  decoration: const InputDecoration(hintText: "Name"),
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                TextField(
+                  controller: quantityController,
+                  keyboardType: TextInputType.number,
+                  decoration: const InputDecoration(hintText: "Quatity"),
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                const SizedBox(
+                  height: 15,
+                ),
+              ],
+            ),
+          ),
+          actions: <Widget>[
+            TextButton(
+              child: const Text('Cancel'),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+            TextButton(
+                child: key == null
+                    ? const Text('Add Item')
+                    : const Text('Update Item'),
+                onPressed: () {
+                  if (key == null) {
+                   Get.find<ShoppingBoxController>().createItem({
+                      "name": nameController.text,
+                      "quantity": quantityController.text
+                    });
 
-//                     nameController.text = "";
-//                     quantityController.text = "";
-//                     Navigator.pop(context);
-//                   }
+                    nameController.text = "";
+                    quantityController.text = "";
+                    Navigator.pop(context);
+                  }
 
-//                   if(key!=null){
-//                     _updateItem(key, {
-//                       'name':nameController.text.trim(),
-//                       'quantity':quantityController.text.trim()
-//                     });
-//                     Navigator.pop(context);
-//                   }
-//                 }),
-//           ],
-//         );
-//       },
-//     );
-//   }
-// }
-// }
+                  if(key!=null){
+                   Get.find<ShoppingBoxController>().updateItem(key, {
+                      'name':nameController.text.trim(),
+                      'quantity':quantityController.text.trim()
+                    });
+                    Navigator.pop(context);
+                  }
+                }),
+          ],
+        );
+      },
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -7,6 +7,7 @@ import 'package:get/get.dart';
 import 'package:hive_db_app/core/controller/shopping_box_controller.dart';
 import 'package:hive_flutter/adapters.dart';
 import 'core/helper/controller-initializer.dart' as di;
+import 'core/widgets/form_dialog_widget.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -68,7 +69,7 @@ class _HomeState extends State<Home> {
                     child: Row(children: [
                       IconButton(
                           onPressed: () {
-                            _showForm(currentItem['key']);
+                            ShoppingHiveFormDialog(context: context, nameController: _nameController,quantityController:_quantityController).showForm(currentItem['key']);
                           },
                           icon: const Icon(
                             Icons.edit,
@@ -92,91 +93,9 @@ class _HomeState extends State<Home> {
             }),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () => _showForm(null),
+        onPressed: () =>ShoppingHiveFormDialog(context: context, nameController: _nameController,quantityController:_quantityController).showForm(null),
         child: const Icon(Icons.add),
       ),
-    );
-  }
-
-  Future<void> _showForm(key) async {
-    if (key != null) {
-      final existingItem =Get.find<ShoppingBoxController>().items.firstWhere((item) => item['key'] == key);
-
-      _nameController.text = existingItem['name'];
-      _quantityController.text = existingItem['quantity'];
-    }
-
-    if (key == null) {
-      _nameController.clear();
-      _quantityController.clear();
-    }
-
-    return showDialog<void>(
-      context: context,
-      barrierDismissible: false, // user must tap button!
-      builder: (BuildContext context) {
-        return AlertDialog(
-          title: key == null
-              ? const Text('Add shopping item')
-              : const Text('Edit shopping item'),
-          content: SingleChildScrollView(
-            child: ListBody(
-              children: <Widget>[
-                TextField(
-                  controller: _nameController,
-                  decoration: const InputDecoration(hintText: "Name"),
-                ),
-                const SizedBox(
-                  height: 10,
-                ),
-                TextField(
-                  controller: _quantityController,
-                  keyboardType: TextInputType.number,
-                  decoration: const InputDecoration(hintText: "Quatity"),
-                ),
-                const SizedBox(
-                  height: 10,
-                ),
-                const SizedBox(
-                  height: 15,
-                ),
-              ],
-            ),
-          ),
-          actions: <Widget>[
-            TextButton(
-              child: const Text('Cancel'),
-              onPressed: () {
-                Navigator.of(context).pop();
-              },
-            ),
-            TextButton(
-                child: key == null
-                    ? const Text('Add Item')
-                    : const Text('Update Item'),
-                onPressed: () {
-                  if (key == null) {
-                   Get.find<ShoppingBoxController>().createItem({
-                      "name": _nameController.text,
-                      "quantity": _quantityController.text
-                    });
-
-                    _nameController.text = "";
-                    _quantityController.text = "";
-                    Navigator.pop(context);
-                  }
-
-                  if(key!=null){
-                   Get.find<ShoppingBoxController>().updateItem(key, {
-                      'name':_nameController.text.trim(),
-                      'quantity':_quantityController.text.trim()
-                    });
-                    Navigator.pop(context);
-                  }
-                }),
-          ],
-        );
-      },
     );
   }
 }
